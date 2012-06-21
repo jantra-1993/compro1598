cd51a7ca5b8582632acf330f04cdebba6ab53b8d
jantra-1993 <yeans_nick@hotmail.com>
2012-06-21 14:07:50 +0700

    new HelloWorld

---
+++ src/th/ac/nu/students/u54341598/compro/ch01/HelloWorld.java
@@ -0,0 +1,8 @@
+package th.ac.nu.students.u54341598.compro.ch01;
+
+public class HelloWorld {
+    public static void main(String[]agrs){
+        System.out.println("HelloWorld");
+        
+    }
+   }
