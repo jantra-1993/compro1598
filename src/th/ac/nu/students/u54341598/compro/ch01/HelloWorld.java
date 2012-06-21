d1127065b2522c7853601281082c3047dcf2d461
jantra-1993 <yeans_nick@hotmail.com>
2012-06-21 14:09:40 +0700

    เพิ่ม "Programming is fun"

--- src/th/ac/nu/students/u54341598/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/students/u54341598/compro/ch01/HelloWorld.java
@@ -3,6 +3,6 @@ package th.ac.nu.students.u54341598.compro.ch01;
 public class HelloWorld {
     public static void main(String[]agrs){
         System.out.println("HelloWorld");
-        
-    }
+        System.out.println("Programming is fun");
+        }
    }
